87166c0022cc480bea5cca2ec8e4a240c7ebba23
Your Name <you@example.com>
2023-02-16 11:11:59 +0700

    tao model

---
+++ myproject/database/migrations/2023_02_16_031811_create_accounts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('Username');
+            $table->string('Password');
+            $table->string('Email');
+            $table->string('Phone');
+            $table->string('Address');
+            $table->string('Fullname');
+            $table->boolean('IsAdmin');
+            $table->string('Avatar');
+            $table->integer('status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
--- myproject/database/migrations/2023_02_16_033940_create_carts_table.php
+++ myproject/database/migrations/2023_02_16_033940_create_carts_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('ProductDetailId');
+            $table->bigInteger('AccountId');
+            $table->Integer('Quantity');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('carts');
     }
 };
